1e51fb47dece6f1d446f7bd19c4e9d69a3af4642
Kos-Mos <ChuyM203@gmail.com>
2020-11-15 16:09:02 -0500

    added service-worker/added webmanifest/added idb.js

---
+++ public/service-worker.js
@@ -0,0 +1,88 @@
+const CACHE_NAME = 'Budget-Cache';
+const DATA_CACHE_NAME = 'Budget-Data-Cache';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  '/js/index.js',
+  '/js/idb.js',
+  '/css/styles.css',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Pre-Cached Successfully: ');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  e.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => {
+      return fetch('/api/transaction').then((response) => {
+        if (response.status === 200) {
+          cache.put('api/transaction', response.clone());
+        }
+        return response;
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Deleteing Cache:', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.url.includes('/api/') && e.request.method === 'GET') {
+    e.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(e.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(e.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(e.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+
+  e.respondWith(
+    fetch(e.request).catch(function () {
+      return caches.match(e.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (e.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/');
+        }
+      });
+    })
+  );
+});
